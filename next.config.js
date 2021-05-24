0e7aff0bd02201c3c4f6dac582f621700d1ebde8
Ghulam Mustafa <mustafaji@gmail.com>
2021-05-24 23:09:05 +0200

    created next config

---
+++ next.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+    env: {
+        DB_LOCAL_URI: ' ',
+        DB_URI: ' ',
+
+        STRIPE_API_KEY: ' ',
+        STRIPE_SECRET_KEY: ' ',
+
+        STRIPE_WEBHOOK_SECRET: 'whsec_2KX5p6VLfvTsRHZv4dclcLjW7ATWilQt',
+
+        CLOUDINARY_CLOUD_NAME: 'hotel-booking',
+        CLOUDINARY_API_KEY: ' ',
+        CLOUDINARY_API_SECRET: ' ',
+
+        SMTP_HOST: "smtp.mailtrap.io",
+        SMTP_PORT: "2525",
+        SMTP_USER: "21f85c3da6b84b",
+        SMTP_PASSWORD: "72cb3edce57f85",
+        SMTP_FROM_EMAIL: "noreply@bookit.com",
+        SMTP_FROM_NAME: "BookIT",
+
+        NEXTAUTH_URL: 'https://bookit.vercel.app',
+    },
+    images: {
+        domains: ['res.cloudinary.com'],
+    },
+}
