a4312a2dd8e7cf625fc587b62739cf769de8aa39
Ghulam Mustafa <mustafaji@gmail.com>
2021-06-12 16:52:49 +0200

    created Footer

---
+++ components/layout/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer className="py-1">
+            <p className="text-center mt-1">
+                Hotel Booking - 2021-2025, All Rights Reserved
+            </p>
+        </footer>
+    )
+}
+
+export default Footer
